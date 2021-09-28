1769e22f92ed1afea20470dbdae2390058205137
hiapellet <hi@apellet.ca>
2021-09-28 13:06:51 -0300

    added testing for model

--- Java/cardealership/src/test/java/com/sg/cardealership/dao/VehicleDaoTest.java
+++ Java/cardealership/src/test/java/com/sg/cardealership/dao/VehicleDaoTest.java
@@ -8,11 +8,9 @@ import com.sg.cardealership.models.Trim;
 import com.sg.cardealership.models.User;
 import com.sg.cardealership.models.Vehicle;
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -115,5 +113,106 @@ public class VehicleDaoTest {
         
         vehicleDao.removeMake(make.getId());
         assertNull(vehicleDao.getMake(make.getId()));
+    }
+    
+    @Test
+    public void testAddAndGetModel() {
+        // Adding User
+        User user = new User("elizbeth@gmail.com",
+                             "Elizbeth",
+                             "Contrera",
+                             "559aead08264d5795d3909718cdd05abd49572e84fe55590eef31a88a08fdffd",
+                             Role.ADMIN);
+        userDao.addUser(user);
+        
+        assertEquals(user, userDao.getUser(user.getEmail()));
+        
+        // Adding Make
+        Make make = new Make("Hyundai", LocalDate.now(), user);
+        vehicleDao.addMake(make);
+        
+        assertEquals(make, vehicleDao.getMake(make.getId()));
+        
+        // Adding Model
+        Model model = new Model("Accent", 
+                                List.of(2015),
+                                LocalDate.now(), 
+                                user, 
+                                make);
+        vehicleDao.addModel(model);
+        
+        assertEquals(model, vehicleDao.getModel(model.getId()));
+    }
+    
+    public void testGetAllModels() {
+        // Adding User
+        User user = new User("elizbeth@gmail.com",
+                             "Elizbeth",
+                             "Contrera",
+                             "559aead08264d5795d3909718cdd05abd49572e84fe55590eef31a88a08fdffd",
+                             Role.ADMIN);
+        userDao.addUser(user);
+        
+        assertEquals(user, userDao.getUser(user.getEmail()));
+        
+        // Adding Make
+        Make make = new Make("Hyundai", LocalDate.now(), user);
+        vehicleDao.addMake(make);
+        
+        assertEquals(make, vehicleDao.getMake(make.getId()));
+        
+        // Adding Models
+        Model firstModel = new Model("Accent", 
+                                     List.of(2015),
+                                     LocalDate.now(), 
+                                     user, 
+                                     make);
+        vehicleDao.addModel(firstModel);
+        
+        Model secondModel = new Model("Azera", 
+                                      List.of(2006, 2007, 2008),
+                                      LocalDate.now(), 
+                                      user, 
+                                      make);
+        vehicleDao.addModel(secondModel);
+        
+        List<Model> models = vehicleDao.getAllModels();
+        
+        assertEquals(2, models.size());
+        assertTrue(models.contains(firstModel));
+        assertTrue(models.contains(secondModel));
+    }
+    
+    @Test
+    public void testRemoveModel() {
+        // Adding User
+        User user = new User("elizbeth@gmail.com",
+                             "Elizbeth",
+                             "Contrera",
+                             "559aead08264d5795d3909718cdd05abd49572e84fe55590eef31a88a08fdffd",
+                             Role.ADMIN);
+        userDao.addUser(user);
+        
+        assertEquals(user, userDao.getUser(user.getEmail()));
+        
+        // Adding Make
+        Make make = new Make("Hyundai", LocalDate.now(), user);
+        vehicleDao.addMake(make);
+        
+        assertEquals(make, vehicleDao.getMake(make.getId()));
+        
+        // Adding Model
+        Model model = new Model("Accent", 
+                                List.of(2015),
+                                LocalDate.now(), 
+                                user, 
+                                make);
+        vehicleDao.addModel(model);
+        
+        assertEquals(model, vehicleDao.getModel(model.getId()));
+        
+        // Remove Model
+        vehicleDao.removeModel(model.getId());
+        assertNull(vehicleDao.getModel(model.getId()));
     }
 }
