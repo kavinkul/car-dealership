aeb60c919eb290d42d615dc4b0c0d3b6f0630040
hiapellet <hi@apellet.ca>
2021-09-30 10:21:18 -0300

    admin service boilerplate code

--- Java/cardealership/src/main/java/com/sg/cardealership/controllers/AdminController.java
+++ Java/cardealership/src/main/java/com/sg/cardealership/controllers/AdminController.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.sg.cardealership.controllers;
 
-/**
- *
- * @author kavin
- */
-public class AdminController {
+import com.sg.cardealership.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    @Autowired
+    AdminService adminService;
+    
+    @GetMapping("/vehicles")
+    public String displayVehicles(Model model) {
+       return "";
+    }
+    
+    @GetMapping("/addvehicle")
+    public String displayAddVehicle(Model model) {
+       return "";
+    } 
+    
+    @GetMapping("/editvehicle/{1}")
+    public String displayEditVehicle(Model model) {
+       return "";
+    }
+    
+    @GetMapping("/users")
+    public String displayUsers(Model model) {
+       return "";
+    }
+    
+    @GetMapping("/adduser")
+    public String displayAddUser(Model model) {
+       return "";
+    }
+    
+    @GetMapping("/edituser")
+    public String displayEditUser(Model model) {
+       return "";
+    } 
+    
+    @GetMapping("/specials")
+    public String displaySpecials(Model model) {
+       return "";
+    } 
 }
---
+++ Java/cardealership/src/main/java/com/sg/cardealership/service/AdminService.java
@@ -0,0 +1,25 @@
+package com.sg.cardealership.service;
+
+import com.sg.cardealership.dao.SpecialsDao;
+import com.sg.cardealership.dao.UserDao;
+import com.sg.cardealership.dao.VehicleDao;
+import com.sg.cardealership.models.Vehicle;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminService {
+    @Autowired
+    VehicleDao vehicleDao;
+    
+    @Autowired
+    UserDao userDao;
+    
+    @Autowired 
+    SpecialsDao specialsDao;
+    
+    public List<Vehicle> getAllVehicles() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
