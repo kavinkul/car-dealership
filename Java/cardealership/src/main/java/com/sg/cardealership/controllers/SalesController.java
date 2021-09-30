a6eb2549e1d5fab7cc02dc0b1a55433f0c977e46
hiapellet <hi@apellet.ca>
2021-09-30 10:34:28 -0300

    sales service boilerplate code

--- Java/cardealership/src/main/java/com/sg/cardealership/controllers/SalesController.java
+++ Java/cardealership/src/main/java/com/sg/cardealership/controllers/SalesController.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.sg.cardealership.controllers;
 
-/**
- *
- * @author kavin
- */
-public class SalesController {
+import com.sg.cardealership.service.SalesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/sales")
+public class SalesController {
+    @Autowired
+    SalesService salesService;
+    
+    @GetMapping("/index")
+    public String displaySales(Model model) {
+       return "";
+    }
+    
+    @GetMapping("/purchaseVehicle/{1}")
+    public String displayPurchaseVehicle(Model model) {
+       return "";
+    }
 }
