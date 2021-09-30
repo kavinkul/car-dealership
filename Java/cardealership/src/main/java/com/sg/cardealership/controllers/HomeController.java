4c9ce6f9ba8d2da7cbc6b472f051c6358b70e05f
hiapellet <hi@apellet.ca>
2021-09-30 10:47:15 -0300

    home service boilerplate code

---
+++ Java/cardealership/src/main/java/com/sg/cardealership/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package com.sg.cardealership.controllers;
+
+import com.sg.cardealership.service.HomeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    @Autowired
+    HomeService homeService;
+    
+    @GetMapping("/index")
+    public String displayFeatured(Model model) {
+        return "";
+    }
+    
+    @GetMapping("/specials")
+    public String displaySpecials(Model model) {
+        return "";
+    }
+    
+    @GetMapping("/contact")
+    public String displayContact(Model model) {
+        return "";
+    }
+}
---
+++ Java/cardealership/src/main/java/com/sg/cardealership/service/HomeService.java
@@ -0,0 +1,25 @@
+package com.sg.cardealership.service;
+
+import com.sg.cardealership.dao.ContactDao;
+import com.sg.cardealership.dao.SpecialsDao;
+import com.sg.cardealership.dao.VehicleDao;
+import com.sg.cardealership.models.Vehicle;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HomeService {
+    @Autowired
+    VehicleDao vehicleDao;
+    
+    @Autowired
+    SpecialsDao specialsDao;
+    
+    @Autowired
+    ContactDao contactDao;
+    
+    public List<Vehicle> getAllFeaturedVehicles() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
